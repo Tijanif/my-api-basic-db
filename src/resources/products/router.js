3fbc1b4010fabbc49826bb3432457b79e54b4de1
Tijani Fulani <Fulani07@gmail.com>
2021-08-02 15:41:38 +0100

    get with id

--- src/resources/products/router.js
+++ src/resources/products/router.js
@@ -6,4 +6,10 @@ productsRouter.get('/', (req, res) => {
   res.json({ products: 'More than you can buy!' });
 });
 
+productsRouter.get('/:id', (req, res) => {
+  const id = req.params.id;
+
+  res.json({ ID: `More than you can buy! ${id}` });
+});
+
 module.exports = productsRouter;
